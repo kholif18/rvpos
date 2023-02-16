27818279a79557f23fb4a2c150520b80e9472173
kholif18 <rahmadnur.kholifatur@gmail.com>
2023-02-16 23:28:33 +0700

    chart dashboard

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -106,17 +106,17 @@
 		<!-- AREA CHART -->
 		<div class="card card-primary card-outline">
 			<div class="card-header">
-			<h3 class="card-title">Mounthly Sales - 2023</h3>
-			<div class="card-tools">
-				<button type="button" class="btn btn-tool" data-card-widget="collapse">
-				<i class="fas fa-minus"></i>
-				</button>
-			</div>
+				<h3 class="card-title">Mounthly Sales - 2023</h3>
+				<div class="card-tools">
+					<button type="button" class="btn btn-tool" data-card-widget="collapse">
+					<i class="fas fa-minus"></i>
+					</button>
+				</div>
 			</div>
 			<div class="card-body">
-			<div class="chart">
-				<canvas id="areaChart" style="min-height: 250px; height: 250px; max-height: 250px; max-width: 100%;"></canvas>
-			</div>
+				<div class="chart">
+					<canvas id="areaChart" style="min-height: 250px; height: 250px; max-height: 250px; max-width: 100%;"></canvas>
+				</div>
 			</div>
 			<!-- /.card-body -->
 		</div>
