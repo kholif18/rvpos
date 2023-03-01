8cb5dc700ea17cc153ac12a53625ebc58c275a51
kholif18 <rahmadnur.kholifatur@gmail.com>
2023-03-01 17:25:30 +0700

    c

--- app/Http/Controllers/Products/Category/StoreController.php
+++ app/Http/Controllers/Products/Category/StoreController.php
@@ -10,7 +10,9 @@ class StoreController extends Controller
     public function index(Request $request)
     {
         $name = $request->input('name');
+        $prefix = $request->input('prefix');
 
         return response()->json(['status' => 'success', 'name' => $name, 'input' => $request->input($name)]);
+        return response()->json(['status' => 'success', 'prefix' => $prefix, 'input' => $request->input($prefix)]);
     }
 }
