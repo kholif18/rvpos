e27d621c8a170858d67c339be68502466b8af214
kholif18 <rahmadnur.kholifatur@gmail.com>
2023-05-13 04:35:36 +0700

    setting

--- resources/views/setting/setting.blade.php
+++ resources/views/setting/setting.blade.php
@@ -1,105 +1,105 @@
 @extends('layouts.master')
 
 @section('title')
-    Setting
+Setting
 @endsection
 
 @section('breadcrumb')
-    @parent
-    <li class="breadcrumb-item active">
-        <a href="{{ url()->current() }}">
-            Setting
-        </a>
-    </li>
+@parent
+<li class="breadcrumb-item active">
+    <a href="{{ url()->current() }}">
+        Setting
+    </a>
+</li>
 @endsection
 
 @section('content')
-    <div class="card card-primary card-outline">
-        <div class="card-header">
-            <div class="card-title">Company Profile</div>
-        </div>
-        <div class="card-body row">
-            <div class=" col-md-5 col-sm-12 col-xs-12 text-center d-flex align-items-center justify-content-center">
+<div class="card card-primary card-outline">
+    <div class="card-header">
+        <div class="card-title">Company Profile</div>
+    </div>
+    <div class="card-body row">
+        <div class=" col-md-5 col-sm-12 col-xs-12 text-center d-flex align-items-center justify-content-center">
+            <div class="">
                 <div class="">
-                    <div class="">
-                        <img src="{{ asset('img/logo.png') }}" alt="" style="width: 150px; margin-bottom: 20px">
-                    </div>
-                    <h2>Ravaa <strong>Studio</strong></h2>
-                    <p><i class="fas fa-map-marker-alt"></i> Gedong, Ds. Ngluyu, Kec. Ngluyu, Kab. Nganjuk<br>
-                        <i class="fas fa-mobile-alt"></i> : 0822-3337-7661 <br>
-                        <i class="fas fa-envelope"></i> : ravaastudio@gmail.com <br>
-                        <i class="fas fa-globe"></i> : ravaastudio.wordpress.com
-                    </p>
+                    <img src="{{ asset('img/logo.png') }}" alt="" style="width: 150px; margin-bottom: 20px">
                 </div>
+                <h2>Ravaa <strong>Studio</strong></h2>
+                <p><i class="fas fa-map-marker-alt"></i> Gedong, Ds. Ngluyu, Kec. Ngluyu, Kab. Nganjuk<br>
+                    <i class="fas fa-mobile-alt"></i> : 0822-3337-7661 <br>
+                    <i class="fas fa-envelope"></i> : ravaastudio@gmail.com <br>
+                    <i class="fas fa-globe"></i> : ravaastudio.wordpress.com
+                </p>
             </div>
-            <div class="col-md-7 col-sm-12 col-xs-12">
-                <form action="{{ url('/setting/add-data') }}" class="form-horizontal" method="post">
-                    <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                    <div class="form-group">
-                        <label class="form-group-sm">Company Name</label>
-                        <div class="input-group input-group-sm">
-                            <div class="input-group-prepend">
-                                <span class="input-group-text" style="width: 30px"><i class="fas fa-landmark"></i></span>
-                            </div>
-                            <input type="text" name="company_name" autofocus class="form-control form-control-sm" value="Ravaa Studio">
+        </div>
+        <div class="col-md-7 col-sm-12 col-xs-12">
+            <form action="{{ url('/setting/add-data') }}" class="form-horizontal" method="post">
+                <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                <div class="form-group">
+                    <label class="form-group-sm">Company Name</label>
+                    <div class="input-group input-group-sm">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" style="width: 30px"><i class="fas fa-landmark"></i></span>
+                        </div>
+                        <input type="text" name="company_name" autofocus class="form-control form-control-sm" value="Ravaa Studio">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="form-group-sm">Address</label>
+                    <div class="input-group input-group-sm">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" style="width: 30px"><i class="fas fa-map-marker-alt"></i></span>
+                        </div>
+                        <input type="text" name="address" class="form-control form-control-sm" value="Gedong, Ds. Ngluyu, Kec. Ngluyu, Kab. Nganjuk">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="form-group-sm">Phone</label>
+                    <div class="input-group input-group-sm">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" style="width: 30px"><i class="fas fa-mobile-alt"></i></span>
+                        </div>
+                        <input type="text" name="phone" class="form-control form-control-sm" value="0822-3337-7661">
+                    </div>
+                </div>
+                <div class="form-group form-group-sm">
+                    <label class="form-group-sm">Company Logo</label>
+                    <div class="input-group input-group-sm">
+                        <div class="custom-file">
+                            <input type="file" name="logo" class="custom-file-input" id="customFile">
+                            <label class="custom-file-label" for="customFile">Choose file</label>
                         </div>
                     </div>
-                    <div class="form-group">
-                        <label class="form-group-sm">Address</label>
-                        <div class="input-group input-group-sm">
-                            <div class="input-group-prepend">
-                                <span class="input-group-text" style="width: 30px"><i class="fas fa-map-marker-alt"></i></span>
-                            </div>
-                            <input type="text" name="address" class="form-control form-control-sm" value="Gedong, Ds. Ngluyu, Kec. Ngluyu, Kab. Nganjuk">
+                </div>
+                <div class="form-group">
+                    <label class="form-group-sm">Email</label>
+                    <div class="input-group input-group-sm">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" style="width: 30px"><i class="fas fa-envelope"></i></span>
                         </div>
+                        <input type="email" name="email" class="form-control form-control-sm" value="ravaastudio@gmail.com">
                     </div>
-                    <div class="form-group">
-                        <label class="form-group-sm">Phone</label>
-                        <div class="input-group input-group-sm">
-                            <div class="input-group-prepend">
-                                <span class="input-group-text" style="width: 30px"><i class="fas fa-mobile-alt"></i></span>
-                            </div>
-                            <input type="text" name="phone" class="form-control form-control-sm" value="0822-3337-7661">
+                </div>
+                <div class="form-group">
+                    <label class="form-group-sm">Website</label>
+                    <div class="input-group input-group-sm">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" style="width: 30px"><i class="fas fa-globe"></i></span>
                         </div>
+                        <input type="text" name="website" class="form-control form-control-sm" value="ravaastudio.wordpress.com">
                     </div>
-                    <div class="form-group form-group-sm">
-                        <label class="form-group-sm">Company Logo</label>
-                        <div class="input-group input-group-sm">
-                            <div class="custom-file">
-                                <input type="file" name="logo" class="custom-file-input" id="customFile">
-                                <label class="custom-file-label" for="customFile">Choose file</label>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <label class="form-group-sm">Email</label>
-                        <div class="input-group input-group-sm">
-                            <div class="input-group-prepend">
-                                <span class="input-group-text" style="width: 30px"><i class="fas fa-envelope"></i></span>
-                            </div>
-                            <input type="email" name="email" class="form-control form-control-sm" value="ravaastudio@gmail.com">
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <label class="form-group-sm">Website</label>
-                        <div class="input-group input-group-sm">
-                            <div class="input-group-prepend">
-                                <span class="input-group-text" style="width: 30px"><i class="fas fa-globe"></i></span>
-                            </div>
-                            <input type="text" name="website" class="form-control form-control-sm" value="ravaastudio.wordpress.com">
-                        </div>
-                    </div>
-                    <div class="form-group text-center align-items-center justify-content-center">
-                        <button type="submit" class="btn btn-success btn-sm save" style="width: 150px"><i class="fas fa-paper-plane"></i> Save</button>
-                        <button type="reset" class="btn btn-danger btn-sm cancel" style="width: 150px; margin-left: 20px"><i class="fas fa-trash"></i> Cancel</button>
-                    </div>
-                </form>
-            </div>
+                </div>
+                <div class="form-group text-center align-items-center justify-content-center">
+                    <button type="submit" class="btn btn-success btn-sm save" style="width: 150px"><i class="fas fa-paper-plane"></i> Save</button>
+                    <button type="reset" class="btn btn-danger btn-sm cancel" style="width: 150px; margin-left: 20px"><i class="fas fa-trash"></i> Cancel</button>
+                </div>
+            </form>
         </div>
     </div>
+</div>
 
-    {{-- printer Setting --}}
-    {{-- <div class="card card-primary card-outline">
+{{-- printer Setting --}}
+{{-- <div class="card card-primary card-outline">
         <div class="card-header">
             <div class="card-title">Printer Setting</div>
         </div>
@@ -185,34 +185,77 @@
 
 
 @push('scripts')
-    <script>
-        $('.cancel').click(function() {
-            Swal.fire({
-                title: 'Are you sure?',
-                text: "You won't be able to revert this!",
-                icon: 'warning',
-                showCancelButton: true,
-                confirmButtonColor: '#3085d6',
-                cancelButtonColor: '#d33',
-                confirmButtonText: 'Yes, cancel it!'
-            });
+<script>
+    $(document).on('click', '.edit-supplier', function(event) {
+        event.preventDefault();
+        document.querySelectorAll('.code_error, .name_error').forEach(function(el) {
+            el.innerHTML = '';
         });
-        // error
-        //----------------------------
-        //         Swal.fire({
-        //   icon: 'error',
-        //   title: 'Oops...',
-        //   text: 'Something went wrong!',
-        //   footer: '<a href="">Why do I have this issue?</a>'
-        // })
-        $('.save').click(function() {
-            Swal.fire({
-                icon: 'success',
-                title: 'Saved!',
-                text: "Your change has been Saved.",
-                showConfirmButton: false,
-                timer: 1500
-            })
+
+        inputName.addEventListener('input', function() {
+            if (this.value.trim() !== '') {
+                // Jika nilai input sudah valid, hapus pesan kesalahan
+                document.querySelector('.name_error').innerHTML = '';
+            }
         });
-    </script>
+
+        // Set url action to edit
+        const httpUrl = $(this).attr('data-url');
+        $(document).find('#form-supplier').attr('action', httpUrl);
+        $(document).find('#form-supplier').attr('method', 'put');
+        $('#supplierModal .modal-title').text('Edit supplier');
+
+        // Fetch data from database
+        const httpFetchUrl = $(this).attr('data-fetch-url');
+        $.ajax({
+            url: httpFetchUrl,
+            success: function(response) {
+                // Do something with the response data
+                console.log(response);
+                if (response.status === 'success') {
+                    const data = response.data;
+                    $(document).find('#form-supplier input[name="code"]').val(data.code);
+                    $(document).find('#form-supplier input[name="name"]').val(data.name);
+                    $(document).find('#form-supplier input[name="phone"]').val(data.phone);
+                    $(document).find('#form-supplier input[name="email"]').val(data.email);
+                    $(document).find('#form-supplier input[name="bank"]').val(data.bank);
+                    $(document).find('#form-supplier input[name="norek"]').val(data.norek);
+                    $(document).find('#form-supplier input[name="address"]').val(data.address);
+                }
+            }
+        });
+
+        // Show modal form
+        $('#supplierModal').modal('show');
+    });
+
+    $('.cancel').click(function() {
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, cancel it!'
+        });
+    });
+    // error
+    //----------------------------
+    //         Swal.fire({
+    //   icon: 'error',
+    //   title: 'Oops...',
+    //   text: 'Something went wrong!',
+    //   footer: '<a href="">Why do I have this issue?</a>'
+    // })
+    $('.save').click(function() {
+        Swal.fire({
+            icon: 'success',
+            title: 'Saved!',
+            text: "Your change has been Saved.",
+            showConfirmButton: false,
+            timer: 1500
+        })
+    });
+</script>
 @endpush
